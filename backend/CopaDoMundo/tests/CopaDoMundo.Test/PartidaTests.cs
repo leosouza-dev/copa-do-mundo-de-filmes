e275ec9bafb8a34b00dc4e72b45244ed77f8504b
sarival <sarival@sarival.com.br>
2020-09-21 13:12:16 -0300

    criando primeiro teste de Partida

---
+++ backend/CopaDoMundo/tests/CopaDoMundo.Test/PartidaTests.cs
@@ -0,0 +1,27 @@
+using CopaDoMundo.Api.Domain.Models;
+using CopaDoMundo.Api.Models;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CopaDoMundo.Test
+{
+    public class PartidaTests
+    {
+
+        [Fact]
+        public void Partida_IniciarPartida_RetornaFilmeComMaiorNota()
+        {
+            // Arrange
+            var filmeA = new Filme { Id = "tt1825683", Titulo = "Pantera Negra", Ano = 2018, Nota = 7.5 };
+            var filmeB = new Filme { Id = "tt3606756", Titulo = "Os Incríveis 2", Ano = 2018, Nota = 8.5 };
+            var partida = new Partida(filmeA, filmeB);
+
+            // Act
+            var vencedor = partida.IniciarPartida();
+
+            // Assert
+            Assert.Equal(filmeB, vencedor);
+        }
+    }
+}
